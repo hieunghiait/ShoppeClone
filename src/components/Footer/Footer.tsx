4448fc19e3732c05dc98994eb672c7b6e0278259
Hieu Nghia Le <85425448+hieunghiait@users.noreply.github.com>
2024-02-26 21:06:29 +0700

    Validation confirm password for register form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
           <div className='lg:col-span-1'>
-            <div>© 2022 Shopee. Tất cả các quyền được bảo lưu.</div>
+            <div>© 2024 Shopee. Tất cả các quyền được bảo lưu.</div>
           </div>
           <div className='lg:col-span-2'>
             <div>
