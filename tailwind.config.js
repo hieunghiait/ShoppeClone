381abf7758f32eef4d17d482643fd368ec837bf5
Hieu Nghia Le <85425448+hieunghiait@users.noreply.github.com>
2024-02-26 21:57:19 +0700

    Custom class container - create  plugin Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
-export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
-export const theme = {
-  extend: {
-    colors: {
-      orange: '#ee4d2d'
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
+  theme: {
+    extend: {
+      colors: {
+        orange: '#ee4d2d'
+      }
     }
-  }
-};
-export const plugins = [];
+  },
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    })
+  ]
+}
